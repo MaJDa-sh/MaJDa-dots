f909731415bf6138b3d1e32bef5086f1dd7dbf7a
MaJDa-sh <temakthe02@gmail.com>
2026-06-08 05:04:31 +0200

    feat(nvim): added nvim-tree as option on starting menu

--- nvim/plugin/mini-starter.lua
+++ nvim/plugin/mini-starter.lua
@@ -1,26 +1,33 @@
 local starter = require("mini.starter")
 
 local logo = [[
-               __     __  __               
-              /  |   /  |/  |              
- _____  ____  $$ |   $$ |$$/  _____  ____  
-/     \/    \ $$ |   $$ |/  |/     \/    \ 
+               __     __  __
+              /  |   /  |/  |
+ _____  ____  $$ |   $$ |$$/  _____  ____
+/     \/    \ $$ |   $$ |/  |/     \/    \
 $$$$$$ $$$$  |$$  \ /$$/ $$ |$$$$$$ $$$$  |
 $$ | $$ | $$ | $$  /$$/  $$ |$$ | $$ | $$ |
 $$ | $$ | $$ |  $$ $$/   $$ |$$ | $$ | $$ |
 $$ | $$ | $$ |   $$$/    $$ |$$ | $$ | $$ |
-$$/  $$/  $$/     $/     $$/ $$/  $$/  $$/ 
+$$/  $$/  $$/     $/     $$/ $$/  $$/  $$/
 ]]
 
 starter.setup({
   evaluate_single = true,
+  header = logo,
+  footer = "",
   items = {
+    -- Toggle NvimTree menu item
+    {
+      name = "Toggle NvimTree",
+      action = function() require('nvim-tree.api').tree.toggle() end,
+      section = "Builtin",
+    },
+    -- existing items
     starter.sections.recent_files(5, false),
     starter.sections.recent_files(5, true),
     starter.sections.sessions(5, true),
     { name = "New file", action = "enew", section = "Builtin" },
-    { name = "Quit", action = "qall", section = "Builtin" },
+    { name = "Quit",     action = "qall", section = "Builtin" },
   },
-  header = logo,
-  footer = "",
 })
